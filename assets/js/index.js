e94a77a1e35deb82ebcddbd4dd15aa763519912b
Grant Burry <grant@burry.me>
2017-04-30 03:07:45 -0600

    Hide loading bar on iOS. :hover fix for iOS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
-/**
- * Main JS file for Casper behaviours
- */
-
 /* globals jQuery, document */
 (function ($, undefined) {
     "use strict";
 
     var $document = $(document);
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     $document.ready(function () {
+        // hide loading bar for MobileSafari
+        if( userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+            $(".pace").hide();
+        }
 
         var $postContent = $(".post-content");
         $postContent.fitVids();
-
     });
 
 })(jQuery);
