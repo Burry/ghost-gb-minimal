0e1785aed1a59509dd6ab60398ef43c8801c87c9
Grant Burry <grant@burry.me>
2017-04-30 19:02:28 -0600

    Oops, javascript were ignored

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/* globals jQuery, document */
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    $document.ready(function () {
+        // iOS adjustments
+        if( userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+            // hide loading bar
+            $(".pace").hide();
+            // add :hover style to all links
+            $("a").click(function() {
+                void(0);
+            });
+        }
+
+        var $postContent = $(".post-content");
+        $postContent.fitVids();
+    });
+
+})(jQuery);
